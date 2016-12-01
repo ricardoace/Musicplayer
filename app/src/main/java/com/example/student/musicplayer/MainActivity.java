7619b1bec70c804a1e25a8cdcecef2d4083f4b27
ricardo acevedo <ricardojracevedo@gmail.com>
2016-12-01 14:52:16 -0800

    first

--- app/src/main/java/com/example/student/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/student/musicplayer/MainActivity.java
@@ -1,13 +1,52 @@
+
 package com.example.student.musicplayer;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.app.Activity;
+        import android.media.MediaPlayer;
+        import android.os.Bundle;
+        import android.os.Handler;
+        import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+        import android.widget.Button;
+        import android.widget.ImageView;
+        import android.widget.SeekBar;
+        import android.widget.TextView;
+        import android.widget.Toast;
+        import java.util.concurrent.TimeUnit;
 
+
+public class MainActivity extends Activity {
+    private Button b1,b2,b3,b4;
+    private ImageView iv;
+    private MediaPlayer mediaPlayer;
+
+    private double startTime = 0;
+    private double finalTime = 0;
+
+    private Handler myHandler = new Handler();;
+    private int forwardTime = 5000;
+    private int backwardTime = 5000;
+    private SeekBar seekbar;
+    private TextView tx1,tx2,tx3;
+
+    public static int oneTimeOnly = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        b1 = (Button) findViewById(R.id.button);
+        b2 = (Button) findViewById(R.id.button2);
+        b3 = (Button)findViewById(R.id.button3);
+        b4 = (Button)findViewById(R.id.button4);
+        iv = (ImageView)findViewById(R.id.imageView);
+
+        tx1 = (TextView)findViewById(R.id.textView2);
+        tx2 = (TextView)findViewById(R.id.textView3);
+        tx3 = (TextView)findViewById(R.id.textView4);
+        tx3.setText("Song.mp3");
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.song);
+        seekbar = (SeekBar)findViewById(R.id.seekBar);
+        seekbar.setClickable(false);
+        b2.setEnabled(false);
